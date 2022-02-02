ca84fc3dc43801b1036ee3a22bb7cc5a52240f6b
Michael Lill <michael.lill@tokiwa.software>
2022-02-02 12:12:47 +0100

    java: enable assertions

--- java/setup.sh
+++ java/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # This file was formerly a part of Julia. License is MIT: https://julialang.org/license
 
+# this enables java assertions
+export MAVEN_OPTS="-ea"
 mvn compile exec:java
 # requires maven and java 7
